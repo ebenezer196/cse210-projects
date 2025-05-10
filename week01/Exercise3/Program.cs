9802729ebc17bb4b5736b837fb80b8ab06cc38e6
Ebenezer <ebenldsgmail.com>
2025-05-10 22:27:43 +0000

    add guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+
+        do
+        {
+            // Create a new random number generator
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101); // 1 to 100
+            int guess;
+            int guessCount = 0;
+
+            Console.WriteLine("I have picked a number between 1 and 100.");
+            
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
