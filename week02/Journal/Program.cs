169ccda15977d6f2d803cb4fbee1ed0df54c01fc
Ebenezer <ebenldsgmail.com>
2025-05-17 20:22:32 +0000

    add journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -12,7 +12,7 @@ class Program
         {
             Console.WriteLine("\nJournal Menu:");
             Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("2. display the journal");
             Console.WriteLine("3. Save the journal to a file");
             Console.WriteLine("4. Load the journal from a file");
             Console.WriteLine("5. Quit");
