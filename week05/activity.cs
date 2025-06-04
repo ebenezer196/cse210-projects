6b4406b67354313dddec0bafbb0a5a0fcf6c1da9
Ebenezer <ebenldsgmail.com>
2025-06-04 22:09:55 +0000

    Add classes

---
+++ week05/activity.cs
@@ -0,0 +1,18 @@
+// BreathingActivity.cs
+public class BreathingActivity
+{
+    private int duration;
+
+    public BreathingActivity()
+    {
+    }
+
+    public void Start()
+    {
+    }
+
+    public string GetDescription()
+    {
+        return "";
+    }
+}
